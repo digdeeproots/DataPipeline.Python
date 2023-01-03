bad95cc91514e106d72259c41a825d32f5a07989
arlobelshee <arlo@belshee.com>
2023-01-03 17:26:52 -0500

    F - Data pipeline segment exists, has an impl, and uses that impl's name when it outputs its display.

--- src/datapipeline/__init__.py
+++ src/datapipeline/__init__.py
@@ -3,3 +3,6 @@
 class PipeSegment:
     def __init__(self, impl):
         self._impl = impl
+
+    def to_verification_string(self):
+        return f'+--{self._impl.name}'
--- tests/pipe_segments.py
+++ tests/pipe_segments.py
@@ -1,7 +1,14 @@
 import pytest
+from assertpy import assert_that
 
 from datapipeline import PipeSegment
-import examplecode
+
+
+class ValidSegmentImpl:
+    name = "Task 1"
+
 
 def test_pipe_segment_names_are_used_when_verifying_them():
-    test_subject = PipeSegment(examplecode.ValidSegmentImpl)
+    processor = ValidSegmentImpl()
+    test_subject = PipeSegment(processor)
+    assert_that(test_subject.to_verification_string()).contains(processor.name)

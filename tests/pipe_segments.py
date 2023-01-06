aa199407f3d6567cb35f37e2d8d852648ab1c5f3
arlobelshee <arlo@belshee.com>
2023-01-06 11:38:05 -0500

    T - Get one basic empty test that calls some product code.

--- tests/pipe_segments.py
+++
@@ -1,6 +0,0 @@
-import pytest
-
-from project.sample_function import this_is_the_function
-
-def test_pipe_segments_have_names():
-    pass
---
+++ tests/test_something.py
@@ -0,0 +1,5 @@
+from  assertpy import assert_that
+from project.examplecode import outer_fun
+
+def test_outer_fun_does_magic():
+    outer_fun()

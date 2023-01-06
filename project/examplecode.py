47a5637925f9fa7fffa04ca55d46b988d143efae
arlobelshee <arlo@belshee.com>
2023-01-06 11:56:54 -0500

    F - Start sketching out the code I'll be getting to.

--- project/examplecode.py
+++ project/examplecode.py
@@ -1,3 +1,43 @@
+from enum import StrEnum
+
+
+class Currency(StrEnum):
+    Dollars = "USD"
+    Pesos = "MXN"
+    Baht = "THB"
+    Ringgit = "MYR"
+    Yuan = "CNY"
+
+class Account:
+    def __init__(self, starting_balance, currency):
+        self.currency = currency
+        self._starting_balance = starting_balance
+        self._committed_transactions = []
+
+    @property
+    def balance(self):
+        return self._starting_balance + sum([t.amount for t in self._committed_transactions])
+
+
+class Loan(Account):
+    def __init__(self, starting_balance):
+        super(Loan, self).__init__(starting_balance)
+
+    @property
+    def balance(self):
+        return -super(Loan, self).balance()
+
+
+class TransactionHalf:
+    def __init__(self, amount: float, account: Account):
+        self.amount = amount
+        self.account = account
+
+
+class Transaction:
+    def __init__(self, source: TransactionHalf, dest: TransactionHalf):
+        self.source = source
+        self.dest = dest
 
 
 def outer_fun():

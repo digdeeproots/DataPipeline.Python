ff6bf2887b5d7e711fe5b7aa6e69f164e0c4ea51
arlobelshee <arlo@belshee.com>
2023-01-17 14:56:36 -0500

    F - now the basics of doing a transform.

--- src/datapipeline/__init__.py
+++ src/datapipeline/__init__.py
@@ -15,10 +15,20 @@ class PipelineStep(Generic[TIn, TOut], metaclass=ABCMeta):
 
 class _PipeSegment(Generic[TIn, TOut]):
     def __init__(self, impl: PipelineStep[TIn, TOut]):
+        self._next_segment = lambda x: None
         self._impl = impl
 
     def to_verification_string(self) -> str:
         return f'+--{self._impl.name}'
+
+    def process(self, input: TIn):
+        self._impl.transform(input)
+        self._next_segment(input)
+
+    def __setattr__(self, key, value):
+        if key == 'next_segment':
+            self._next_segment = value
+        super(_PipeSegment, self).__setattr__(key, value)
 
 
 class DataProcessingSegment(_PipeSegment[T, T], Generic[T]):
--- tests/pipe_segments.py
+++ tests/pipe_segments.py
@@ -4,11 +4,32 @@ from assertpy import assert_that
 from datapipeline import DataProcessingSegment
 
 
+class DataTransferObject:
+    def __init__(self):
+        self.some_num = None
+        self.dumb_obj = None
+
+
 class ValidSegmentImpl:
     name = "Task 1"
 
+    def transform(self, input):
+        input.dumb_object = f"{{ saw: {input.some_num} }}"
 
 def test_pipe_segment_names_are_used_when_verifying_them():
     processor = ValidSegmentImpl()
     test_subject = DataProcessingSegment(processor)
     assert_that(test_subject.to_verification_string()).contains(processor.name)
+
+def test_pipe_segment_calls_its_transform_impl_to_process_data():
+    processor = ValidSegmentImpl()
+    test_subject = DataProcessingSegment(processor)
+    input = DataTransferObject()
+    input.some_num = 4
+    result = None
+    def store_next(input):
+        nonlocal result
+        result = input
+    test_subject.next_segment = store_next
+    test_subject.process(input)
+    assert_that(result.dumb_object).is_equal_to("{ saw: 4 }")

d993177a25934560fcdfca65c88c314d71e6b835
arlobelshee <arlo@belshee.com>
2023-01-24 17:46:50 -0500

    F!! Can now differentiate between restructuring steps and processing steps. The static type checking might work (no promises), but the very basic runtime checking is validated.

--- src/datapipeline/__init__.py
+++ src/datapipeline/__init__.py
@@ -1,39 +1,107 @@
+from __future__ import annotations
+
 from abc import ABCMeta, abstractmethod
-from typing import TypeVar, Generic
+from typing import TypeVar, Generic, runtime_checkable, Protocol
 
 T = TypeVar('T')
+U = TypeVar('T')
 TIn = TypeVar('TIn')
 TOut = TypeVar('TOut')
 
 
-class PipelineStep(Generic[TIn, TOut], metaclass=ABCMeta):
+@runtime_checkable
+class NamedStep(Protocol):
     @property
     @abstractmethod
     def name(self) -> str:
         pass
 
 
-class _PipeSegment(Generic[TIn, TOut]):
-    def __init__(self, impl: PipelineStep[TIn, TOut]):
-        self._next_segment = lambda x: None
-        self._impl = impl
+@runtime_checkable
+class ProcessingStep(NamedStep, Protocol[TIn]):
+    @abstractmethod
+    def transform(self, data: TIn) -> None:
+        pass
+
+
+@runtime_checkable
+class RestructuringStep(NamedStep, Protocol[TIn, TOut]):
+    @abstractmethod
+    def restructure(self, data: TIn) -> TOut:
+        pass
+
+
+class _PipeSegment(Generic[TIn, TOut], metaclass=ABCMeta):
+    _next_segment: _PipeSegment[TOut, U]
+
+    def __init__(self):
+        self._next_segment = NullTerminator()
+
+    @property
+    @abstractmethod
+    def name(self) -> str:
+        pass
 
     def to_verification_string(self) -> str:
-        return f'+--{self._impl.name}'
+        return f'+--{self.name}'
 
-    def process(self, data: TIn):
-        self._impl.transform(data)
-        self._next_segment(data)
+    @abstractmethod
+    def _process(self, data: TIn) -> TOut:
+        pass
 
-    def __setattr__(self, key, value):
-        if key == 'next_segment':
-            self._next_segment = value
-        super(_PipeSegment, self).__setattr__(key, value)
+    def process(self, data: TIn) -> None:
+        result = self._process(data)
+        self._next_segment.process(result)
+
+    def then(self, next_segment: ProcessingStep[TOut] | RestructuringStep[TOut, U]) -> _PipeSegment[TOut, U]:
+        self._next_segment = DataProcessingSegment(next_segment) if isinstance(next_segment, ProcessingStep) else \
+            RestructuringSegment(next_segment)
+        return self._next_segment, next_segment
 
 
 class DataProcessingSegment(_PipeSegment[T, T], Generic[T]):
-    pass
+    _impl: ProcessingStep[T]
+
+    def __init__(self, impl: ProcessingStep[T]):
+        super(DataProcessingSegment, self).__init__()
+        self._impl = impl
+
+    @property
+    def name(self) -> str:
+        return self._impl.name
+
+    def _process(self, data: TIn) -> TOut:
+        self._impl.transform(data)
+        return data
 
 
 class RestructuringSegment(_PipeSegment[TIn, TOut], Generic[TIn, TOut]):
-    pass
+    _impl: RestructuringStep[TIn, TOut]
+
+    def __init__(self, impl: RestructuringStep[TIn, TOut]):
+        super(RestructuringSegment, self).__init__()
+        self._impl = impl
+
+    @property
+    def name(self) -> str:
+        return self._impl.name
+
+    def _process(self, data: TIn) -> TOut:
+        return self._impl.restructure(data)
+
+
+class NullTerminator(_PipeSegment[T, T], Generic[T]):
+    def __init__(self):
+        pass  # Don't call the superclass.
+
+    def name(self) -> str:
+        return "End"
+
+    def process(self, data: TIn) -> None:
+        pass
+
+    def _process(self, data: TIn) -> TOut:
+        raise NotImplementedError
+
+    def then(self, next_segment: ProcessingStep[T] | RestructuringStep[T, U]) -> _PipeSegment[T, U]:
+        raise NotImplementedError
--- tests/pipe_segments.py
+++ tests/pipe_segments.py
@@ -16,20 +16,28 @@ class ValidSegmentImpl:
     def transform(self, data):
         data.dumb_object = f"{{ saw: {data.some_num} }}"
 
+
+class CapturingSink:
+    name = "Capture for test"
+
+    def __init__(self):
+        self.result = None
+
+    def transform(self, data):
+        self.result = data
+
+
 def test_pipe_segment_names_are_used_when_verifying_them():
     processor = ValidSegmentImpl()
     test_subject = DataProcessingSegment(processor)
     assert_that(test_subject.to_verification_string()).contains(processor.name)
+
 
 def test_pipe_segment_calls_its_transform_impl_to_process_data():
     processor = ValidSegmentImpl()
     test_subject = DataProcessingSegment(processor)
     data = DataTransferObject()
     data.some_num = 4
-    result = None
-    def store_next(input):
-        nonlocal result
-        result = input
-    test_subject.next_segment = store_next
+    _, sink = test_subject.then(CapturingSink())
     test_subject.process(data)
-    assert_that(result.dumb_object).is_equal_to("{ saw: 4 }")
+    assert_that(sink.result.dumb_object).is_equal_to("{ saw: 4 }")
